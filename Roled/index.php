d12fdde82cba886bc1f0a767248b76e09170904f
nenpav <nereapazv@gmail.com>
2024-05-07 20:52:52 +0200

    modal login lista, falta responsive

--- Roled/index.php
+++ Roled/index.php
@@ -1,8 +1,8 @@
-<!--<?php
+<?php
     //Comprobar si existe una sesion de usuario iniciada
         //Existe -> redireccion a pagina de inicio del usuario
         //No existe -> se mantiene en esta pagina
--->
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,6 +11,7 @@
     <title>Roled</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./src/css/index.css">
+    <link rel="stylesheet" href="./src/css/modales.css">
 </head>
 <body>
     <header>
@@ -19,9 +20,9 @@
         </figure>
         <nav>
             <ul id="menuSinSesion">
-                <li><a href="#" aria-label="Enlace a index" class="activa">Inicio<span></span></a></li>
-                <li><a href="" aria-label="Enlace a login">Login<span></span></a></li>
-                <li><a href="" aria-label="Enlace a registro">Sign up<span></span></a></li>
+                <li><a href="" aria-label="Enlace a index" class="activa">Inicio<span></span></a></li>
+                <li><button id="openModalSesion">Login<span></span></button></li>
+                <li><button>Sign up<span></span></button></li>
             </ul>
             <!-- Menú con sesión -->
         </nav>
@@ -127,27 +128,31 @@
         </ul>
       </section>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <dialog id="modalsesion">
+    <dialog id="modalSesion">
+      
       <section id="left">
         <h3>Iniciar Sesión</h3>
         <p>¿No tienes cuenta?</p>
-        <button id="registro">Sign up</button>
+        <button class="boton" id="registro">Sign up</button>
       </section>
       <section id="rigth">
+      <button id="cerrar">X</button>
+      <svg xmlns="http://www.w3.org/2000/svg" height="95px" viewBox="0 -960 960 960" width="95px" fill="#FFFFFF"><path d="M222-255q63-44 125-67.5T480-346q71 0 133.5 23.5T739-255q44-54 62.5-109T820-480q0-145-97.5-242.5T480-820q-145 0-242.5 97.5T140-480q0 61 19 116t63 109Zm257.81-195q-57.81 0-97.31-39.69-39.5-39.68-39.5-97.5 0-57.81 39.69-97.31 39.68-39.5 97.5-39.5 57.81 0 97.31 39.69 39.5 39.68 39.5 97.5 0 57.81-39.69 97.31-39.68 39.5-97.5 39.5Zm.66 370Q398-80 325-111.5t-127.5-86q-54.5-54.5-86-127.27Q80-397.53 80-480.27 80-563 111.5-635.5q31.5-72.5 86-127t127.27-86q72.76-31.5 155.5-31.5 82.73 0 155.23 31.5 72.5 31.5 127 86t86 127.03q31.5 72.53 31.5 155T848.5-325q-31.5 73-86 127.5t-127.03 86Q562.94-80 480.47-80Zm-.47-60q55 0 107.5-16T691-212q-51-36-104-55t-107-19q-54 0-107 19t-104 55q51 40 103.5 56T480-140Zm0-370q34 0 55.5-21.5T557-587q0-34-21.5-55.5T480-664q-34 0-55.5 21.5T403-587q0 34 21.5 55.5T480-510Zm0-77Zm0 374Z"/></svg>
         <form action="" method="POST">
           <p>
               <label for="user">Usuario</label>
               <input type="text" name="user" id="user">
           </p>
           <p>
-              <label for="pwd"></label>
-              <input type="pwd" name="pwd" id="pwd">
+              <label for="pwd">Contraseña</label>
+              <input type="password" name="pwd" id="pwd">
           </p>
-          <input type="submit" value="Login">
+          <input class="boton" id="login" type="submit" value="Login">
        </form>
       </section>
     </dialog>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="./src/js/modales.js"></script>
 
 </body>
 </html>
--- Roled/src/js/modales.js
+++ Roled/src/js/modales.js
@@ -1,2 +1,22 @@
 const $d = document
-         
+      $botonLogin = $d.querySelector("#openModalSesion"),
+      $modalSesion = $d.querySelector("#modalSesion"),
+      $cerrarModalSesion = $d.querySelector("#cerrar")
+
+//console.log($botonLogin)
+//console.log($modalSesion)
+
+function openModal(el, idModal){
+    el.addEventListener("click",el=>{
+        idModal.showModal()
+    })
+}
+
+function closeModal(el, idModal){
+    el.addEventListener("click",el=>{
+        idModal.close()
+    })
+}
+
+openModal($botonLogin, $modalSesion)
+closeModal($cerrarModalSesion, $modalSesion)
