cd7c62a785ba192e9308c981cd740487ea8e6ff6
nenpav <nereapazv@gmail.com>
2024-06-14 20:58:13 +0200

    arreglo cambio avatar

--- Roled/src/js/miPerfil.js
+++ Roled/src/js/miPerfil.js
@@ -1,8 +1,18 @@
 const $d2=document,
-      $botonVerMas = $d2.querySelector("#mas")
+      $botonVerMas = $d2.querySelector("#mas"),
+      $avatar = $d2.querySelector(".img"),
+      $cargar = $d2.querySelector("#cargar")
 
       $botonVerMas.addEventListener("click",e=>{
         e.preventDefault()
         window.location.href = "./design.php"
-    
+     })
+
+     $cargar.addEventListener("change",e=>{
+        e.preventDefault()
+        const nuevaImg = e.target.files[0]
+        if(nuevaImg){
+                const objectURL = URL.createObjectURL(nuevaImg)
+                $avatar.style.backgroundImage = `url(${objectURL})`
+        }
      })
