fedb66481f80bfb254e38d20727eb8111417ac8d
nenpav <nereapazv@gmail.com>
2024-06-17 19:55:46 +0200

    añadidos registros para pruebas

--- Roled/src/scriptsBBDD/insercion.sql
+++ Roled/src/scriptsBBDD/insercion.sql
@@ -2,4 +2,17 @@ USE roled;
 
 INSERT INTO usuarios (username, pdHash, email, tipo, avatar, fechaNac) values ('user123','$2y$10$qDzwqw0UgDEauokHTvniTOz3Oewx1C5Q53LqkjNYsWgXCjFgmqKNa', 'ejemplo@ejemplo.com','U', '1995-02-05');
 
-INSERT INTO design (id_usuario, nombre) values (1, sfsd)
+INSERT INTO design (id_usuario, nombre) values (1, dibujo1.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo2.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo3.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo4.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo5.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo6.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo7.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo8.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo9.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo10.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo12.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo13.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo14.svg);
+INSERT INTO design (id_usuario, nombre) values (1, dibujo15.svg);
