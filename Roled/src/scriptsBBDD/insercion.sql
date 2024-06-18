ecf3e2e8e921c56b8ecc4c44f72402adc4838b28
nenpav <nereapazv@gmail.com>
2024-06-18 10:14:20 +0200

    arreglo sql de insercion

--- Roled/src/scriptsBBDD/insercion.sql
+++ Roled/src/scriptsBBDD/insercion.sql
@@ -1,18 +1,18 @@
 USE roled;
 
-INSERT INTO usuarios (username, pdHash, email, tipo, avatar, fechaNac) values ('user123','$2y$10$qDzwqw0UgDEauokHTvniTOz3Oewx1C5Q53LqkjNYsWgXCjFgmqKNa', 'ejemplo@ejemplo.com','U',NULL, '1995-02-05');
+INSERT INTO usuarios (username, pwdHash, email, tipo, avatar, fecha_nac) values ('user123','$2y$10$qDzwqw0UgDEauokHTvniTOz3Oewx1C5Q53LqkjNYsWgXCjFgmqKNa', 'ejemplo@ejemplo.com','U',NULL, '1995-02-05');
 
-INSERT INTO design (id_usuario, nombre) values (1, dibujo1.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo2.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo3.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo4.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo5.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo6.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo7.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo8.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo9.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo10.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo12.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo13.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo14.svg);
-INSERT INTO design (id_usuario, nombre) values (1, dibujo15.svg);
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo1.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo2.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo3.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo4.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo5.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo6.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo7.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo8.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo9.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo10.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo12.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo13.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo14.svg');
+INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo15.svg');
