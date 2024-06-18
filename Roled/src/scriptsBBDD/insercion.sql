1e752ac27006bbe705775b9a45461ae94701817a
nenpav <nereapazv@gmail.com>
2024-06-18 21:31:44 +0200

    añadir script

--- Roled/src/scriptsBBDD/insercion.sql
+++ Roled/src/scriptsBBDD/insercion.sql
@@ -1,6 +1,6 @@
 USE roled;
 
-INSERT INTO usuarios (username, pwdHash, email, tipo, avatar, fecha_nac) values ('user123','$2y$10$qDzwqw0UgDEauokHTvniTOz3Oewx1C5Q53LqkjNYsWgXCjFgmqKNa', 'ejemplo@ejemplo.com','U',NULL, '1995-02-05');
+INSERT INTO usuarios (username, pwdHash, email, tipo, avatar, fecha_nac) values ('user123','$2y$10$4Ao54.9.PhKyZ0Cx5rq97uYvfqB78qhS57eHj.sFwMSj48lGDEYJu', 'ejemplo@ejemplo.com','U',NULL, '1995-02-05');
 
 INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo1.svg');
 INSERT INTO design (id_usuario, nombre) values ('user123', 'dibujo2.svg');
