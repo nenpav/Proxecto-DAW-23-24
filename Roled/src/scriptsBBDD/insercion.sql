e38c28f738aa91afd94f6df1ec4e8586d2ea643c
nenpav <nereapazv@gmail.com>
2024-06-18 10:08:27 +0200

    arrreglo script

--- Roled/src/scriptsBBDD/insercion.sql
+++ Roled/src/scriptsBBDD/insercion.sql
@@ -1,6 +1,6 @@
 USE roled;
 
-INSERT INTO usuarios (username, pdHash, email, tipo, avatar, fechaNac) values ('user123','$2y$10$qDzwqw0UgDEauokHTvniTOz3Oewx1C5Q53LqkjNYsWgXCjFgmqKNa', 'ejemplo@ejemplo.com','U', '1995-02-05');
+INSERT INTO usuarios (username, pdHash, email, tipo, avatar, fechaNac) values ('user123','$2y$10$qDzwqw0UgDEauokHTvniTOz3Oewx1C5Q53LqkjNYsWgXCjFgmqKNa', 'ejemplo@ejemplo.com','U',NULL, '1995-02-05');
 
 INSERT INTO design (id_usuario, nombre) values (1, dibujo1.svg);
 INSERT INTO design (id_usuario, nombre) values (1, dibujo2.svg);
